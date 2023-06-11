60fe779e2c9996de28217e4cd2e309bda5be3dd5
fiqri0718 <75074846+fiqri0718@users.noreply.github.com>
2023-06-11 07:57:17 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,12 +9,43 @@ import NotFound from "./Routes/NotFound";
 import "./CSS/style.css";
 
 const App = () => {
+    const [students, setStudents] = useState([]);
+  const [loading, setLoading] = useState(false);
+  let navigate = useNavigate();
+  
+    const renderStudents = async () => {
+    try {
+      const respone = await fetch("http://localhost:3001/student");
+      const result = await respone.json();
+      setStudents(result);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
+
+  const handleadddata = async (student) => {
+    try {
+      await fetch("http://localhost:3001/student", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(student),
+      });
+      setLoading(true);
+      renderStudents();
+      navigate("/student");
+    } catch (error) {}
+  };
+  
   return (
     <>
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/add" element={<AddStudent />} />
+        <Route path="/add" element={<AddStudent createStudent={handleadddata}/>} />
         <Route path="/student" element={<Student />} />
         <Route path="/student/:id" element={<EditStudent />} />
         <Route path="/*" element={<NotFound />} />
